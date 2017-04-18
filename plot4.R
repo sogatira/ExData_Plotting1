3290fa2edbee323917232a6dacbcf5d46376450e
sogatira <sogatira@gmail.com>
2017-04-18 23:14:22 +0200

    añadimos graficos

--- plot4.R
+++ plot4.R
@@ -34,12 +34,12 @@ par(mfrow = c(2,2))
 
 with(df, {
   plot(datetime, Global_active_power, type = "l", main = "", xlab = "", ylab = "Global Active Power (kilowatts)")
-  plot(datetime, Voltage, type = "l", main = "", xlab = "")
-  plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+  plot(datetime, Voltage, type = "l", main = "", xlab = "datetime")
+  plot(datetime, Sub_metering_1, type = "l", xlab = "datetime", ylab = "Energy sub metering")
   lines(datetime, Sub_metering_2, col = "red")
   lines(datetime, Sub_metering_3, col = "blue")
-  legend("topright", lty = c(1,1,1), col = c("black","red","blue"), legend = names(df)[7:9], cex = 0.65, bty = "n")
-  plot(datetime, Global_reactive_power, type = "l", main = "", xlab = "", ylab = "Global Reactive Power")
+  legend("topright", lty = c(1,1,1), col = c("black","red","blue"), legend = names(df)[7:9], bty = "n")
+  plot(datetime, Global_reactive_power, type = "l", main = "", xlab = "", ylab = "Global_Reactive_Power")
 }) 
 
 dev.off()
